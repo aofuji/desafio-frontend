bb83f95b5616d4155438c8135f0c51a94ce43846
Andre Ofuji <30282153+aofuji@users.noreply.github.com>
2024-10-27 14:05:34 -0300

    Rename folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LayoutComponent } from './components/layout/layout.component';
+import { LayoutComponent } from './main/layout/layout.component';
 
 @Component({
   selector: 'app-root',
