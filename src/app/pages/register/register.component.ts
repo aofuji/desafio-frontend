5467b232b8dcd85a3ed24ac53a3ff6d4dc0fda36
Andre Ofuji <30282153+aofuji@users.noreply.github.com>
2024-10-27 14:42:50 -0300

    Add component shared list and form

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../../shared/components/form/form.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
